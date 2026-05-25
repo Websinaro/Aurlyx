1dcb0c7f61ca61de41639e0abbf7261546f8bae3
Adith .v.s <websinaro@gmail.com>
2026-05-25 07:02:43 +0530

    Update MediaCard.kt

--- app/src/main/java/com/auralyx/ui/components/MediaCard.kt
+++ app/src/main/java/com/auralyx/ui/components/MediaCard.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.*
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
